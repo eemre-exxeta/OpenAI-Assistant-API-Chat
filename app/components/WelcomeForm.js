e2851907361d40c4ab988463ae018a1fb25f9dcb
Ekrem Emre <ekrem.emre@exxeta.com>
2023-11-20 17:32:15 +0100

    some fixes in naming

--- app/components/WelcomeForm.js
+++ app/components/WelcomeForm.js
@@ -54,11 +54,14 @@ const WelcomeForm = ({
   } else if (progress !== lastProgress) {
     setLastProgress(progress);
   }
+
+  console.log(assistantName, assistantDescription);
+
   return (
     <div className="border-gray-500 bg-gray-200 sm:mx-0 mx-5 mt-20 max-w-screen-md rounded-md border-2 sm:w-full">
       <div className="flex flex-col space-y-4 p-7 sm:p-10">
         <h1 className="text-lg font-semibold text-black">
-          Welcome to the Wrapper for OpenAI Assistant API!
+          Welcome to "<a href="https://platform.openai.com/docs/assistants/overview">OpenAI Assistant API</a>"-Wrapper!
         </h1>
         <form className="flex flex-col space-y-3">
           <input
